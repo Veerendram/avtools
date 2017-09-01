1a54523d041a60efdf03e836a110e50cd55f9c90
vmalyavantham <rama2908>
2017-09-01 18:44:50 +0530

    Remove unused utilities filelib and netlib

--- avtools/__init__.py
+++ avtools/__init__.py
@@ -10,8 +10,6 @@ from avtools.image_tools.decode_qr_codes import QrCode
 from avtools.image_tools.create_qrcode import CreateQRcode
 from avtools.image_tools.crop_images import CropImages
 
-from avtools.utils.fileLib import fileLib
-from avtools.utils.netLib import netLib
 from avtools.utils.ncat import Netcat
 from avtools.utils.processInfo import ProcessInfo
 from avtools.utils.registry import RegistryHandler
--- avtools/utils/__init__.py
+++ avtools/utils/__init__.py
@@ -1,5 +1,3 @@
-from fileLib import fileLib
-from netLib import netLib
 from ncat import Netcat
 from processInfo import ProcessInfo
 from registry import RegistryHandler
